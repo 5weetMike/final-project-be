05e36f7a9fe58bfccd87b020098ac9a68bab538a
Daniel Worthington <daniel.worthington96@outlook.com>
2024-10-10 15:43:20 +0100

    bug: re-add function to find all

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -7,14 +7,23 @@ const signup = async (req, res) => {
             email: req.body.email,
             password: req.body.password,
           });
-        
+        console.log(user);
       res.status(201).json({message: "success", user: user})
     } catch (error) {
         res.status(501).json({message: error.message, error: error})
     }
   }
 
-  module.exports = {
-    signup: signup,
-
+  const allUsers = async (req, res) => {
+    try {
+      const users = await User.find({});
+      res.status(201).json({message: "success", users: users});
+    } catch (error) {
+      res.status(501).json({message: error.message, error: error});
+    }
   };
+
+module.exports = {
+  signup: signup,
+  allUsers: allUsers,
+};
