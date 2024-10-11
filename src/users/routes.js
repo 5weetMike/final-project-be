b77fc953c21fd3fd7d8b502ebf8ddf1135916098
Samm Wiltshire <samm94tw@gmail.com>
2024-10-11 13:49:36 +0100

    chore: deleteUser

--- src/users/routes.js
+++ src/users/routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 const userRouter = Router();
 
-const {signup, allUsers} = require("./controllers");
+const {signup, allUsers, deleteUser} = require("./controllers");
 
 // signup
 userRouter.post("/users/signup", signup);
 
 //Find all Users
 userRouter.get("/users/allusers", allUsers);
+
+userRouter.del("/user/delete", deleteUser);
 
 module.exports = userRouter;
