9a6f00e805c8365a4f19b4e96d99330e665715d6
Michael Hampson <mdavidhampson@gmail.com>
2024-10-11 12:11:10 +0100

    chore: debugging: adding console logs

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -10,6 +10,7 @@ const signup = async (req, res) => {
         console.log(user);
       res.status(201).json({message: "success", user: user})
     } catch (error) {
+      console.log("error", error)
         res.status(501).json({message: error.message, error: error})
     }
   }
@@ -19,6 +20,7 @@ const signup = async (req, res) => {
       const users = await User.find({});
       res.status(201).json({message: "success", users: users});
     } catch (error) {
+      console.log("error", error)
       res.status(501).json({message: error.message, error: error});
     }
   };
