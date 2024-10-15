21f74acb813bb94203af44e105b483e860ec1ecd
Michael Hampson <mdavidhampson@gmail.com>
2024-10-15 15:06:13 +0100

    enhancement: add corsorigin

--- src/server.js
+++ src/server.js
@@ -13,7 +13,9 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+const corsOrigin =process.env.ORIGIN
+
+app.use(cors({origin: corsOrigin}));
 
 app.get("/.netlify/functions/api/health", (req, res) => {
     res.status(200).json({ message: "API is healthy" });
