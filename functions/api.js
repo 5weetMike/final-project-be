f381d4b3617305676dcb0139f0c96b69a389c11d
Michael Hampson <mdavidhampson@gmail.com>
2024-10-16 10:24:18 +0100

    bugfix: cors errors

--- functions/api.js
+++ functions/api.js
@@ -5,6 +5,22 @@ const connection = require("../src/db/connection");
 const app = require("../src/server");
  
 const port = process.env.PORT || 5001;
+
+const origin = process.env.ORIGIN
+const whitelist = [origin]
+
+app.use(express.json());
+
+const corsOrigin = {origin:function(origin,callback){
+  if(whitelist.includes(origin)){
+    callback(null,true)
+  }else{
+    console.log("origin: ", origin, " not allowed")
+    callback(new Error("not allowed by cors"))
+  }
+}, credentials:true}
+
+app.use(cors(corsOrigin));
  
 app.listen(port, () => {
   connection();
--- src/server.js
+++ src/server.js
@@ -11,21 +11,21 @@ const port = process.env.PORT || 5001;
 
 const app = express();
 console.log(process.env.ORIGIN)
-const origin = process.env.ORIGIN
-const whitelist = [origin]
+// const origin = process.env.ORIGIN
+// const whitelist = [origin]
 
-app.use(express.json());
+// app.use(express.json());
 
-const corsOrigin = {origin:function(origin,callback){
-  if(whitelist.includes(origin)){
-    callback(null,true)
-  }else{
-    console.log("origin: ", origin, " not allowed")
-    callback(new Error("not allowed by cors"))
-  }
-}, credentials:true}
+// const corsOrigin = {origin:function(origin,callback){
+//   if(whitelist.includes(origin)){
+//     callback(null,true)
+//   }else{
+//     console.log("origin: ", origin, " not allowed")
+//     callback(new Error("not allowed by cors"))
+//   }
+// }, credentials:true}
 
-app.use(cors(corsOrigin));
+// app.use(cors(corsOrigin));
 
 app.get("/.netlify/functions/api/health", (req, res) => {
     res.status(200).json({ message: "API is healthy" });
