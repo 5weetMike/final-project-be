3b7d09dcb6a79f783fc024a5ba3ec1fff6cfb9dd
Daniel Worthington <daniel.worthington96@outlook.com>
2024-10-16 12:05:02 +0100

    chore: resolve conflicts

---
+++ src/dogs/controllers.js
@@ -0,0 +1,23 @@
+const Dog = require("./model");
+const User = require("../users/model");
+
+const addDogs = async (req, res) => {
+    try {
+        const dog = await Dog.create({
+            owner: req.body.ownerId,
+            name: req.body.name,
+            breed: req.body.breed,
+            age: req.body.age,
+            size: req.body.size,
+            toy: req.body.toy,
+        });
+        const updatedUser = await User.findByIdAndUpdate(ownerId, { $push: { dogs: newDog._id } });
+        res.status(201).json({message: "success", dogs: dog, updateduser: updatedUser});
+    } catch (error) {
+        res.status(501).json({message: error.message, error: error});
+    }
+};
+
+module.exports = {
+    addDogs: addDogs,
+};
--- src/users/model.js
+++ src/users/model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
       type: String,
       required: true
     },
+    dogs: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Dog', // Reference to the Dog model
+        }],
   });
   
   const User = mongoose.model("User", userSchema);
